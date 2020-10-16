a997ce4a0bc19cdc936a4d92123d9ecf7acd771c
Sundar Machamasi <unimaxmacha@gmail.com>
2020-10-16 17:20:40 +0900

    file updated
    2020-10-16
    added codes in resetPassword.php

--- admin/resetPassword.php
+++ admin/resetPassword.php
@@ -1,1 +1,106 @@
-Reset password page.
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+ 
+// Include config file
+require_once "../includes/config.php";
+ 
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "パスワードを入力してください。";     
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "パスワードは6文字以上である必要があります。";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "確認パスワードを入力してください。";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "パスワードが一致しませんでした。";
+        }
+    }
+        
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE tbl_users SET password = ? WHERE id = ?";
+        
+        if($stmt = $dbConn->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("si", $param_password, $param_id);
+            
+            /* Set the "cost" parameter to 12. */
+			$options = ['cost' => 12];
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT, $options); // Create a password hash
+            $param_id = $_SESSION["id"];
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
+    
+    // Close connection
+    $dbConn->close();
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>パスワード変更</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2>パスワード変更</h2>
+        <p>パスワードをリセットするには、このフォームに記入してください。</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+            <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                <label>新しパスワード：</label>
+                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+                <span class="help-block"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>パスワード確認：</label>
+                <input type="password" name="confirm_password" class="form-control">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="提出">
+                <a class="btn btn-link" href="welcome.php">キャンセル</a>
+            </div>
+        </form>
+    </div>    
+</body>
+</html>
