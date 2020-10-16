2d9b3936043f5c5990a1f489f6b6db7dc46d7eb4
Sundar Machamasi <unimaxmacha@gmail.com>
2020-10-16 13:43:44 +0900

    File updated
    File Updated: 2020-10-16
    1. make functional for register page with mysql
    2. config.php - added timezone and utf8
    3. style.css - width updated

--- includes/config.php
+++ includes/config.php
@@ -5,9 +5,12 @@
 	define('DB_USERNAME', 'root');
 	define('DB_PASSWORD', '');
 	define('DB_NAME', 'db_dbinfo');
-
+	date_default_timezone_set('Asia/Tokyo');
 	/* Attempt to connect to MySQL database */
 	$dbConn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	$dbConn->set_charset("utf8");
+	//$dbConn->exec("SET time_zone = 'Asia/Tokyo'");
+
 
 	/* Check Connection */
 	if($dbConn === false) {
