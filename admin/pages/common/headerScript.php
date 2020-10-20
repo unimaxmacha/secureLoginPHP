8fc18c1c43f44f374831c859f5cd708ac6eadd02
Sundar Machamasi <unimaxmacha@gmail.com>
2020-10-20 16:00:23 +0900

    New File added
    2020-10-20
    1. headerScript.php
    2. list.php

---
+++ admin/pages/common/headerScript.php
@@ -0,0 +1,17 @@
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="<?=base_url()?>assets/plugins/fontawesome-free/css/all.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="<?=base_url()?>assets/dist/css/adminlte.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+
+<script>
+	$(document).ready(function() {
+		$('#example').DataTable();
+	} );
+</script>
